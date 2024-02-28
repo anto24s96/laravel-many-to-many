472c4f2125726cd9a9db93a85de76fc60dc034f6
anto24s96 <146432921+anto24s96@users.noreply.github.com>
2024-02-28 15:18:53 +0100

    Initial commit

---
+++ app/Http/Requests/StoreProjectRequest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreProjectRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'name'        => 'required|max:50',
+            'type_id'     => 'nullable|exists:types,id',
+            'description' => 'required',
+            'start_date'  => 'required',
+            'end_date'    => 'required',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'name.required'        => 'Il campo Nome deve essere obbligatorio',
+            'name.max'             => 'Il campo Nome può avere al massimo 50 caratteri',
+            'type_id.exists'       => 'La tipologia selezionata non esiste',
+            'description.required' => 'Il campo Description deve essere obbligatorio',
+            'start_date.required'  => 'Il campo Inizio Data deve essere obbligatorio',
+            'end_date.required'    => 'Il campo Fine Data deve essere obbligatorio',
+        ];
+    }
+}
---
+++ database/seeders/TypeSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Models\Type;
+use Illuminate\Support\Str;
+
+class TypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for ($i = 0; $i < 10; $i++) {
+            $new_type = new Type;
+            $new_type->name = $faker->word();
+            $new_type->slug = Str::slug($new_type->name, '-');
+
+            $new_type->save();
+        }
+    }
+}
